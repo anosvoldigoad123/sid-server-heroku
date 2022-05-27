2ca4620fba4c646fcca4a12c2d5655ff0f7f00e6
aminobot22 <76242458+aminobot22@users.noreply.github.com>
2022-05-27 13:38:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ async def submit_report(request: Request):
     return data
     
 if __name__ == "__main__":
-    uvicorn.run("main:app",host="0.0.0.0")
+    uvicorn.run("app:app",host="0.0.0.0")
