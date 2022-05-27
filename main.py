aaa693d5e2bd39f66cd7dedd16b9949236fbca8a
aminobot22 <76242458+aminobot22@users.noreply.github.com>
2022-05-27 13:29:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,102 @@
-from threading import Thread
-from pyngrok import ngrok
-from flask import Flask
-from os import system
-from os import environ
-app=Flask(__name__)
-Thread(target=system, args=("proxy --hostname=127.0.0.1 --port=8000", )).start()
-ngrok.set_auth_token("29hIZG0nfarmYevtd77rbSVXIac_3L9AtC7GctnomGBg4i8Mz")
-tcp=ngrok.connect(8000, "tcp")
-@app.route("/")
-def index():
-    return str(tcp.public_url)
-app.run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))
+import json
+import urllib.request
+from fastapi import FastAPI, Request
+import json
+from fastapi.responses import JSONResponse
+import uvicorn
+import random
+import os
+import sys
+from time import time as timestamp
+from time import sleep
+import names
+from hashlib import sha1
+from functools import reduce
+from base64 import b85decode, b64decode
+import random
+import aiohttp
+import asyncio
+import requests
+import hmac
+import platform,socket,re,uuid
+import base64
+import  threading
+from uuid import uuid4
+import aminos
+
+def r():
+    s = requests.Session()
+    return s.headers['User-Agent']
+def sigg(data):
+        key='f8e7a61ac3f725941e3ac7cae2d688be97f30b93'
+        mac = hmac.new(bytes.fromhex(key), data.encode("utf-8"), sha1)
+        digest = bytes.fromhex("42") + mac.digest()
+        return base64.b64encode(digest).decode("utf-8")
+def dev():
+    hw=(names.get_full_name()+str(random.randint(0,10000000))+platform.version()+platform.machine()+names.get_first_name()+socket.gethostbyname(socket.gethostname())+':'.join(re.findall('..', '%012x' % uuid.getnode()))+platform.processor())
+    identifier=sha1(hw.encode('utf-8')).digest()
+    key='02b258c63559d8804321c5d5065af320358d366f'
+    mac = hmac.new(bytes.fromhex(key), b"\x42" + identifier, sha1)
+    return (f"42{identifier.hex()}{mac.hexdigest()}").upper()
+
+def reset(l):
+  #data.subClient.send_message(data.chatId,"done")
+  #sleep(1)
+  sys.argv
+  sys.executable
+  print("restart now")
+  os.execv(sys.executable, ['python'] + sys.argv)
+
+def login_custom(email: str, password: str,device:str):
+        headers = {
+            #"NDCDEVICEID": '223B063D54BEB7463B92A073735DB6F26EFD413010CCF78271F5953F8BB9010FCFF94D3FF917CB98DE',
+            "Accept-Language": "en-US",
+            "Content-Type": "application/json; charset=utf-8",
+            "User-Agent": r(),
+            "Host": "service.narvii.com",
+            "Accept-Encoding": "gzip",
+            "Connection": "Keep-Alive"
+        }
+        data = json.dumps({
+            "email": email,
+            # "phoneNumber":email,
+            "v": 2,
+            "secret":f"0 {password}",
+            "deviceID": device,
+            "clientType": 100,
+            "action": "normal",
+            "timestamp": int(timestamp() * 1000)
+        })
+        headers["NDC-MSG-SIG"]=sig(data)
+        headers["NDCDEVICEID"]=dev()
+        response = requests.post(f"{api}/g/s/auth/login", headers=headers, data=data)
+        if response.status_code == 403:
+        	error=json.loads({"api:statuscode":000,"api:message":"wait ip is changing"})
+        	return error
+        else:
+        	return response.json()
+        
+app = FastAPI()
+
+@app.get('/')
+async def get_webpage():
+    return "Sid server"
+    
+@app.get('/reset')
+async def res():
+    threading.Thread(target=reset,args=[""]).start()
+    return "restart"
+
+
+
+@app.post("/login")
+async def submit_report(request: Request):
+    body = await request.json()
+    email=body["email"]
+    password=body["password"]
+    device=body["device"]
+    data=login_custom(email, password, device)
+    return data
+    
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="0.0.0.0")
